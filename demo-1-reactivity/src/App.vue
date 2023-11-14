c789214d1614924147ecd0a863ef7bde51be0393
cr0wg4n <mcm.crw@gmail.com>
2023-11-14 00:51:09 -0400

    demo 1

---
+++ demo-1-reactivity/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import DemoReactivity from './components/DemoReactivity.vue';
+</script>
+
+<template>
+  <DemoReactivity />
+</template>
+
---
+++ demo-1-reactivity/src/components/DemoReactivity.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(0)
+console.log('Inicializando', {
+  counter: counter.value
+})
+
+setInterval(()=>{
+  counter.value++
+}, 1000)
+</script>
+
+<template>
+  <div>
+    counter: {{ counter }}
+  </div>
+</template>
