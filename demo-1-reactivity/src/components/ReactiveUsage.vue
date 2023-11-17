c1e85faaa201fdedaffec0335f4f2410e06c020c
cr0wg4n <mcm.crw@gmail.com>
2023-11-16 23:32:59 -0400

    feat, reactivity duel

---
+++ demo-1-reactivity/src/components/ReactiveUsage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { reactive } from 'vue'
+
+  // Inicialización de variable reactiva
+  const user = reactive({
+    name: 'Samuel Lopez',
+    age: 24,
+    email: 'samuL@demo.com'
+  })
+  // Para modificar un valor interno
+  user.age = 23
+  // Para acceder a un valor interno
+  console.log(user.name)
+  // No imprime el objecto, imprime un Proxy ¿?
+  console.log(user) 
+</script>
+
+<template>
+  <div>
+    {{ user.name }}
+  </div>
+</template>
---
+++ demo-1-reactivity/src/components/RefUsage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  // Inicialización de variable reactiva
+  const counter = ref(0)
+  // Para modificar
+  counter.value = 10
+  // Para acceder (imprime 10)
+  console.log(counter.value) 
+</script>
+
+<template>
+  <div>
+    {{ counter }}
+  </div>
+</template>
