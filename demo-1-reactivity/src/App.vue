b223fcc5729a5c8d37f270588f53ce08573706f2
Mauricio M <mcm.crw@gmail.com>
2023-11-17 16:59:43 -0400

    fix, composables

--- demo-1-reactivity/src/App.vue
+++ demo-1-reactivity/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import DemoReactivity from './components/DemoReactivity.vue';
 import DemoNoReactivity from './components/DemoNoReactivity.vue';
+import ReactiveUsage from './components/ReactiveUsage.vue';
+import RefUsage from './components/RefUsage.vue';
+import MouseDemo from './components/MouseDemo.vue';
 </script>
 
 <template>
     <DemoReactivity />
     <!-- Uncomment the next line to see what happens if we don't use reactivity vars -->
     <!-- <DemoNoReactivity /> -->
+    <!-- <ReactiveUsage /> -->
+    <!-- <RefUsage /> -->
+    <!-- <MouseDemo /> -->
 </template>
 
